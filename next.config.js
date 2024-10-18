8b44dd0d4d0afc9411f6f699ee2d8522c49958bd
cescgreoles <cesc.greoles@gmail.com>
2024-10-18 11:44:12 +0200

    changes of next.config

--- next.config.js
+++ next.config.js
@@ -1,23 +1,17 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "example.com",
-        port: "",
         pathname: "/path-to-images/**",
       },
     ],
   },
-
   eslint: {
     ignoreDuringBuilds: true,
   },
-
   typescript: {
     ignoreBuildErrors: true,
   },
